5ee807857edac39b326989a5858bcc8838d5e5c7
Jalo Moster <jlvmoster@gmail.com>
2019-02-22 18:09:34 -0500

    Initial setup of website

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  // get the header height programmatically
+  var headerHeight = $('#navigation').outerHeight();
+
+  // fix body offset from the header section
+  $('body').attr('data-offset', headerHeight + 'px');
+  $('body').css('padding-top', headerHeight + 'px');
+
+  // fix section anchor offset from the header section
+  $('a[href^="#"]').each(function () {
+    var href = $(this).attr('href');
+    $(`${href}`).css('padding-top', headerHeight + 'px');
+    $(`${href}`).css('margin-top', -headerHeight + 'px');
+  });
+});
